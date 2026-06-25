5daeb02312dd5069923fd60a31d0b4705b473ba2
Daniela <sofiagamboa198@gmail.com>
2026-06-24 21:10:28 -0500

    Creación de estructura visual

---
+++ Frontend/src/pages/Destinos.jsx
@@ -0,0 +1,13 @@
+function Destinos(){
+
+return(
+
+<h1>
+Destinos
+</h1>
+
+)
+
+}
+
+export default Destinos;
---
+++ Frontend/src/pages/Inicio.jsx
@@ -0,0 +1,23 @@
+import Button from "../components/Button";
+
+function Inicio(){
+
+return(
+
+<div>
+
+<h1>
+Los mejores viajes
+</h1>
+
+<Button
+texto="Reservar"
+/>
+
+</div>
+
+)
+
+}
+
+export default Inicio;
