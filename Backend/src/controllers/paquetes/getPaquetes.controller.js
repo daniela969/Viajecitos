93b8476b21fc26dbf5f083b241a1bbd7e10c1dc6
RaulilloSierra <raulitosierradiaz@gmail.com>
2026-07-23 10:01:27 -0500

    ajustes en datos de destino y paquetes + tablas relacionadas

--- Backend/src/controllers/paquetes/getPaquetes.controller.js
+++ Backend/src/controllers/paquetes/getPaquetes.controller.js
@@ -1,8 +1,16 @@
-import { Paquete } from "../../models/index.js";
+import { Destino, Paquete } from "../../models/index.js";
 
 export async function getPaquetes(req, res) {
   try {
-    const paquetes = await Paquete.findAll();
+    const paquetes = await Paquete.findAll({
+      attributes: {
+        exclude: ["destinoId"],
+      },
+      include: {
+        model: Destino,
+        as: "destino"
+      }
+    });
     res.json(paquetes);
   } catch (error) {
     res.status(500).json({
--- Backend/src/data/dataPaquete.js
+++ Backend/src/data/dataPaquete.js
@@ -1,6 +1,8 @@
 const dataPaquete = [
   {
     nombre: "Camino de Santiago – “Una ruta de historia y reflexión”",
+    descripción:
+      "El Camino de Santiago es una de las rutas de peregrinación más importantes del mundo, con siglos de historia y tradición. Sus diferentes recorridos atraviesan paisajes naturales, pueblos medievales y ciudades llenas de patrimonio cultural hasta llegar a Santiago de Compostela, en Galicia, donde se encuentra la catedral que guarda los restos del apóstol Santiago.Más que un simple viaje, el Camino ofrece una experiencia única de conexión con la naturaleza, la cultura y uno mismo. Durante el recorrido, los visitantes pueden disfrutar de senderos, arquitectura histórica, gastronomía regional y el encuentro con peregrinos de diferentes países. Su combinación de historia, espiritualidad y aventura lo convierte en un destino turístico reconocido internacionalmente.",
     incluye: [
       "Alojamiento en hoteles y albergues para peregrinos.Recorridos por tramos destacados del Camino de Santiago",
       "Guía acompañante durante la ruta",
@@ -9,13 +11,16 @@ const dataPaquete = [
       "Experiencia gastronómica con platos tradicionales de Galicia.",
     ],
     duracion: "7 días / 6 noches",
-    precio: "7000",
+    precio: "1049",
     imagen:
       "https://santiagogate.com/media/k2/items/cache/2e8bf6f45fb2b01f7bc63e367c45fa81_L.jpg?t=20240531_165955",
+    destinoId: "6351abd7-ff2d-4a5c-a7d3-77a55b1bdace",
   },
 
   {
     nombre: "Carnaval de Venecia – “Magia, tradición y elegancia”",
+    descripción:
+      "El Carnaval de Venecia es una de las celebraciones culturales más famosas del mundo, reconocida por sus elegantes disfraces, máscaras tradicionales y su ambiente lleno de historia. Tiene sus orígenes en la antigua República de Venecia y se caracteriza por sus desfiles, eventos artísticos y celebraciones en las calles y plazas de la ciudad. Durante el carnaval, Venecia se transforma en un escenario lleno de color, música y misterio, donde los visitantes pueden disfrutar de la arquitectura histórica, los canales y la tradición de las famosas máscaras venecianas. Esta festividad combina arte, cultura y entretenimiento, convirtiéndose en un destino turístico único que atrae a personas de todo el mundo",
     incluye: [
       "Alojamiento en hotel cercano al centro histórico",
       "Transporte turístico y recorridos por la ciudad",
@@ -25,25 +30,74 @@ const dataPaquete = [
       "Tour gastronómico con comida típica italiana.",
     ],
     duracion: "5 días / 4 noches",
-    precio: "7000",
+    precio: "2099",
     imagen:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0rzWiuD8d_7CH14udCDfbZUfnVQhUN84VmQ&s",
+    destinoId: "859cef9b-b4b7-492a-890d-38ed2678b5e9",
   },
 
   {
-    nombre: "Bahía Solano – “Encuentro con la naturaleza del Pacífico”",
+    nombre: "Bogotá Night Experience",
+    descripción:
+      "Vive la capital colombiana como nunca antes con un recorrido lleno de música, gastronomía, cultura y entretenimiento. Durante tres días disfrutarás de los mejores bares, discotecas y zonas de rumba de Bogotá, sin dejar de conocer algunos de sus principales atractivos turísticos. Un paquete ideal para quienes buscan diversión, nuevas experiencias y conocer personas de diferentes partes del mundo.",
     incluye: [
-      "Alojamiento en ecohoteles o cabañas naturales",
-      "Transporte local y acompañamiento de guías comunitarios",
-      "Participación en jornada de liberación de tortugas marinas (según temporada)",
-      "Caminatas ecológicas por selva y playas del Pacífico",
-      "Avistamiento de fauna y actividades de turismo de naturaleza",
-      "Alimentación con gastronomía típica de la región.",
+      "2 noches de alojamiento en hotel de 4 estrellas",
+      "Desayunos diarios",
+      "Traslados aeropuerto – hotel – aeropuerto.",
+      "City Tour por La Candelaria, Plaza de Bolívar y Museo del Oro",
+      "Ascenso a Monserrate",
+      "Tour nocturno por la Zona T y Parque de la 93",
+      "Entrada VIP a una de las mejores discotecas de la ciudad",
+      "Seguro de viaje",
     ],
-    duracion: "4 días / 3 noches",
-    precio: "7000",
+    duracion: "3 días / 2 noches",
+    precio: "449",
     imagen:
-      "https://imagenes2.eltiempo.com/files/image_1200_535/uploads/2024/10/01/66fbeee7a4f85.jpeg",
+      "https://imagenes2.eltiempo.com/files/image_1200_535/files/fp/uploads/2023/07/28/64c3e13fe6948.r_d.1592-1360.jpeg",
+    destinoId: "aa414ee8-d8f8-4533-9607-869df8d06e0d",
+  },
+  {
+    nombre: "Bogotá en Familia",
+    descripción:
+      "Comparte una semana inolvidable descubriendo los principales atractivos de Bogotá y sus alrededores. Este paquete combina cultura, naturaleza, entretenimiento y descanso para ofrecer actividades pensadas para todas las edades, permitiendo crear recuerdos únicos junto a quienes más quieres.",
+    incluye: [
+      "6 noches de alojamiento en hotel de 4 estrellas",
+      "Desayunos diarios",
+      "Traslados aeropuerto – hotel – aeropuerto",
+      "City Tour por el centro histórico",
+      "Entrada al Museo del Oro y Museo Botero",
+      "Ascenso a Monserrate",
+      "Excursión de un día a la Catedral de Sal de Zipaquirá",
+      "Visita al Jardín Botánico y al Parque Simón Bolívar",
+      "Día libre para compras o actividades personales",
+      "Seguro de viajes",
+    ],
+    duracion: "7 días / 6 noches",
+    precio: "1095",
+    imagen:
+      "https://ambienteurbano.co/wp-content/uploads/2018/02/Simon-Bolivar-2.jpg",
+    destinoId: "aa414ee8-d8f8-4533-9607-869df8d06e0d",
+  },
+  {
+    nombre: "Bogotá de Fe y Tradición",
+    descripción:
+      "Recorre algunos de los lugares de mayor importancia espiritual e histórica de Bogotá y sus alrededores. Este paquete está diseñado para quienes desean combinar la fe con el patrimonio cultural, visitando templos emblemáticos, santuarios y monumentos religiosos en un ambiente de tranquilidad y reflexión.",
+    incluye: [
+      "4 noches de alojamiento en hotel de 4 estrellas.",
+      "Desayunos diarios",
+      "Traslados aeropuerto – hotel – aeropuerto",
+      "Ascenso y visita al Santuario de Monserrate.",
+      "Recorrido por las principales iglesias históricas de La Candelaria.",
+      "Excursión de un día a la Catedral de Sal de Zipaquirá",
+      "Visita al Santuario del Divino Niño del 20 de Julio",
+      "Tiempo libre para actividades de reflexión o compras de artículos religiosos",
+      "Seguro de viajes",
+    ],
+    duracion: "5 días / 4 noches",
+    precio: "749",
+    imagen:
+      "https://bogota.gov.co/sites/default/files/styles/1050px/public/field/image/catedralprimadabogota.jpg",
+    destinoId: "aa414ee8-d8f8-4533-9607-869df8d06e0d",
   },
 ];
 
